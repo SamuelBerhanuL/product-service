d78faab91eaec661920b7d767a2c0892c8ac641b
USER <samberhanuclassno67@gmail.com>
2026-06-03 10:13:34 +0300

    feat: introduce DTO pattern with ProductRequest and ProductResponse

--- src/main/java/com/ctbe/product_service/repository/ProductRepository.java
+++ src/main/java/com/ctbe/product_service/repository/ProductRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByCategory(String category);
-
+    List<Product> findByNameContainingIgnoreCase(String keyword);
 }
